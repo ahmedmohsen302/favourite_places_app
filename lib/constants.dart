f3776b4e1243f6b7a5ad06a8d447945f70a93795
ahmedmohsen302 <ahmedthemohsen@gmail.com>
2025-05-14 20:22:11 +0300

    theming done with google fonts package added

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color(0xFF910282),
+  brightness: Brightness.dark,
+);
---
+++ lib/views/places_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PlacesView extends StatelessWidget {
+  const PlacesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Your Places',
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.primary,
+            fontSize: 26,
+          ),
+        ),
+      ),
+    );
+  }
+}
